6ca26c160b21ac9295178762725a62bbd0525333
Ramesh Pingili <rameshpingili777@gmail.com>
2026-07-15 12:29:13 -0400

    Bump app version to 1.0.1

--- app.config.js
+++ app.config.js
@@ -7,7 +7,7 @@ export default {
     slug: "bullsignalsai",
     owner: "bullsignalsai",
     scheme: "alphaclara",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "dark",
